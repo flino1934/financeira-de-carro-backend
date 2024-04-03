42f2b7a65701564cb3c5326a427e752fcde8bc2a
Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>
2024-04-03 14:15:01 -0300

    implementando insert de clientes
    Signed-off-by: Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>

--- mobiauto/src/main/java/com/mobiauto/controllers/ClientController.java
+++ mobiauto/src/main/java/com/mobiauto/controllers/ClientController.java
@@ -1,14 +1,17 @@
 package com.mobiauto.controllers;
 
 import com.mobiauto.dto.ClienteDTO;
+import com.mobiauto.dto.RevendaDTO;
 import com.mobiauto.services.ClienteServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clientes")
@@ -21,5 +24,26 @@ public class ClientController {
     public ResponseEntity<Page<ClienteDTO>> findAll(Pageable pageable) {
         Page<ClienteDTO> list = service.findAllPaged(pageable);
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClienteDTO> findById(@PathVariable long id) {
+
+        ClienteDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> insert(@Valid @RequestBody ClienteDTO dto) {
+
+
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+
     }
 }
--- mobiauto/src/main/java/com/mobiauto/services/impl/ClientServiceImpl.java
+++ mobiauto/src/main/java/com/mobiauto/services/impl/ClientServiceImpl.java
@@ -4,17 +4,23 @@ import com.mobiauto.dto.ClienteDTO;
 import com.mobiauto.entites.Cliente;
 import com.mobiauto.repositories.ClienteRepository;
 import com.mobiauto.services.ClienteServiceInterface;
+import com.mobiauto.services.exceptions.ResourceNotFoundExceptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientServiceImpl implements ClienteServiceInterface {
 
     @Autowired
     private ClienteRepository repository;
+
     @Override
+    @Transactional(readOnly = true)
     public Page<ClienteDTO> findAllPaged(Pageable pageable) {
 
         Page<Cliente> list = repository.findAll(pageable);
@@ -23,12 +29,26 @@ public class ClientServiceImpl implements ClienteServiceInterface {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public ClienteDTO findById(long id) {
-        return null;
+
+
+        Optional<Cliente> obj = repository.findById(id);
+        Cliente entity = obj.orElseThrow(() -> new ResourceNotFoundExceptions("Cliente não encontrado"));
+
+        return new ClienteDTO(entity);
     }
 
     @Override
-    public ClienteDTO insert(ClienteDTO ClienteDTO) {
-        return null;
+    public ClienteDTO insert(ClienteDTO clienteDTO) {
+
+        Cliente entity = new Cliente();
+        entity.setNome(clienteDTO.getNome());
+        entity.setEmail(clienteDTO.getEmail());
+        entity.setTelefone(clienteDTO.getTelefone());
+
+        repository.save(entity);
+
+        return new ClienteDTO(entity);
     }
 }
